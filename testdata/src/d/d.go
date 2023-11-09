15d062e75fc11000afb3a7a971e6094d7aa51fca
tomato3713 <tomato3713@hatena.ne.jp>
2023-11-09 22:27:51 +0900

    update testdata

--- testdata/src/d/d.go
+++ testdata/src/d/d.go
@@ -1,15 +1,15 @@
-package main
+package d
 
 import "fmt"
 
 var AError = fmt.Errorf("A")
 var BError = fmt.Errorf("B")
 
-func afunc() error { // want "return errors: main.AError"
+func afunc() error { // want "return errors: d.AError"
 	return AError
 }
 
-func bfunc(b int) error { // want "return errors: main.AError, main.BError"
+func bfunc(b int) error { // want "return errors: d.AError, d.BError"
 	if b == 0 {
 		return afunc()
 	}
